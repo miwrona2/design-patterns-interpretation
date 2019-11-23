503157e989d45fc3cb3982917deb557e0d5bd836
miwrona2 <miwrona2@interia.pl>
2019-11-23 13:16:42 +0100

    observer php: example with order and statistics chart

--- php/Observer.php
+++ php/Observer.php
@@ -1,14 +1,36 @@
 <?php
 
-class Subject {
+/**
+ * Class Order in eCommerce system
+ */
+class Order implements \SplSubject
+{
+
+    const STATUS_PENDING = 1;
+    const STATUS_COMPLETED = 2;
 
     /**
      * @var array
      *
      */
-    private $observer;
+    private $observers;
 
-    //business logic
+    /**
+     * @var ?int
+     */
+    private $status;
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?int $status): Order
+    {
+        $this->status = $status;
+        $this->notify();
+        return $this;
+    }
 
     public function notify(): void
     {
@@ -16,15 +38,44 @@ class Subject {
             $observer->update($this);
         }
     }
-}
 
-interface ObserverInterface {
-    public function update();
-}
-
-class ExampleObserver implements ObserverInterface {
-    public function update()
+    public function attach(SplObserver $observer)
     {
+        $this->observers[] = $observer;
+    }
+
+    public function detach(SplObserver $observer)
+    {
+        unset($observer, $this->observers);
+    }
+}
+
+class StatisticChart implements \SplObserver
+{
+    public function update(SplSubject $subject)
+    {
+        /**
+         * @var Order $subject
+         */
+
+        if ($subject->getStatus() === Order::STATUS_COMPLETED) {
+            $this->drawChart($subject);
+        }
+    }
+
+    private function drawChart(Order $order): void
+    {
+        // refresh chart that displays number of orders in last week, last month period, etc.
+        print 'chart rebuilt';
     }
 
 }
+
+// usage
+$order = new Order();
+$statisticChart = new StatisticChart();
+$order->attach($statisticChart);
+$order->setStatus(Order::STATUS_COMPLETED);
+
+// when any Order status is changed to 'completed' chart with statistics is rebuilt
+
